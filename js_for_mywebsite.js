f49d7a43d3942688b58f398657757a2ca94f74ef
Shashi Bhushan Kanishk <155289171+sbkanishk@users.noreply.github.com>
2024-05-28 16:08:52 +0530

    Delete js_for_mywebsite.js

--- js_for_mywebsite.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('nav ul li a');
-    
-    links.forEach(link => {
-        link.addEventListener('click', function(event) {
-            alert(`You clicked on ${event.target.textContent}!`);
-        });
-    });
-});
